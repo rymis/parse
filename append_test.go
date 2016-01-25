6d3ac14a3926c7435cd293e1caf761374a5f9e53
Mikhail Ryzhov <rymiser@gmail.com>
2016-01-25 11:15:25 +0300

    Value writers

---
+++ append_test.go
@@ -0,0 +1,106 @@
+package parse
+
+import (
+	"testing"
+	"fmt"
+)
+
+type spaces struct {
+	Spaces string `regexp:"[ \\n\\t\\r]*"`
+}
+
+type white struct {
+	White string `regexp:"([ \\t\\r\\n]*|#[^\n]*\n)*"`
+}
+
+type config struct {
+	Sections []section `repeat:"*"`
+	W          white
+	Eof        string `not_any:"true" regexp:".|\\n"`
+}
+
+type section struct {
+	White      white
+	Name       string `regexp:"[a-zA-Z][a-zA-Z0-9_]*"`
+	W1         white
+	_          string `literal:"{"`
+	Pairs    []pair
+	W2         spaces
+	_          string `literal:"}"`
+}
+
+type pair struct {
+	White      white
+	Name       string `regexp:"[a-zA-Z][a-zA-Z0-9_]*"`
+	W1         spaces
+	_          string `literal:"="`
+	W2         spaces
+	Value      value
+	W3          spaces
+}
+
+type value struct {
+	FirstOf
+	Int        int64
+	String     string
+	Bool       bool
+	Array      array
+	RawString  string `regexp:"[^\n]*\n"`
+}
+
+type array struct {
+	_          string `literal:"["`
+	Values   []value `delimiter:","`
+	_          string `literal:"]"`
+}
+
+func skip(str []byte, loc int) int {
+	for i := loc; i < len(str); i++ {
+		if str[i] != ' ' && str[i] != '\t' && str[i] != '\r' {
+			return i
+		}
+	}
+
+	return len(str)
+}
+
+var test1 string = `
+Section0 {
+	name1 = [ 1   ,  2 , 3 ]
+	name2 = 2
+	name3 = "String"
+	name4 = true
+}
+
+Section1 {
+	int = -5
+	bool = false
+	string = "Hello, world!"
+	raw_string = this is raw string
+	array = [ 1, 2, false ]
+}
+
+Section2 {
+	test = [ 1, true, [ 2, false ], "The End!" ]
+}
+
+`
+
+func TestAppend(t *testing.T) {
+	var cfg config
+	nl, err := Parse(&cfg, []byte(test1), &Options{Debug:true})
+	if err != nil {
+		fmt.Printf("Error(%d): %v\n", nl, err)
+		return
+	}
+	println(nl)
+
+	res, err := Append(nil, cfg)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	fmt.Printf("CONFIG:\n%s\n", string(res))
+}
+
